7089d5601a2d121517959741242f1d0d5d296032
Bruno Lima <brunnolima64@gmail.com>
2024-07-31 19:42:56 -0300

    first commit

---
+++ src/controllers/todo.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { prisma } from "../libs/prisma";
+
+export const getAllTodos = async () => {
+    return await prisma.todo.findMany({});
+}
+
+export const addTodo = async (title: string, done: boolean) => {
+
+    if(title && title.length >= 2) {
+        const newTodo = await prisma.todo.create({
+            data: {
+                title: title,
+                done: false
+            }
+        })
+
+        return newTodo;
+    } 
+    
+}
+
+export const updateTitleTodo = async (id: number, title: string, done: boolean) => {
+
+    if(title && title.length >= 2) {
+        return await prisma.todo.update({
+            where: { id },
+            data: { title: title }
+        });
+    }
+
+    if (done === false) {
+        return await prisma.todo.update({
+            where: { id },
+            data: { done: false }
+        });
+    } else {
+        return await prisma.todo.update({
+            where: { id },
+            data: { done: true }
+        });
+    }
+}
+
+export const removeTodo = async (id: number) => {
+    return await prisma.todo.delete({
+        where: { id }
+    })
+}
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,36 @@
 import { Router } from 'express';
+import { addTodo, getAllTodos, removeTodo, updateTitleTodo } from '../controllers/todo';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
+});
+
+mainRouter.get('/', async (req, res) => {
+    const results = await getAllTodos();
+    res.json({ results: results });
+});
+
+mainRouter.post('/todo', async (req, res) => {
+    if(!req.body.title) return false;
+
+    const results = await addTodo(req.body.title);
+    res.status(201).json({ results: results });
+});
+
+mainRouter.put('/todo/:id', async (req, res) => {
+    if(!req.params.id) return false;
+
+    let id = parseInt(req.params.id);
+    const results = await updateTitleTodo(id, req.body.title, req.body.done);
+    res.status(202).json({ results: results });
+});
+
+mainRouter.delete('/todo/:id', async (req, res) => {
+    if(!req.params.id) return false;
+
+    let id = parseInt(req.params.id);
+    const results = await removeTodo(id);
+    res.status(201).json({ results: results });
 });
